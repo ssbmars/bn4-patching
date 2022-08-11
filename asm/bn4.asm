f71ced195885c5634db8a2eaee73f0c5220e12ce
ssbmars <46971717+ssbmars@users.noreply.github.com>
2022-08-10 18:54:57 -0700

    potential fix for circlegun segfault
    and other places in the rom where it appears the same function can cause a segfault read. 10/12 of these modifications are untested because I don't know what calls them, but I'm sure it will be fine :thumbsup:

--- asm/bn4.asm
+++ asm/bn4.asm
@@ -6,3 +6,31 @@
 // do stuff
 
 .org 0x08000000 //hardcoded address (usually these should be avoided if you're modifying both versions of a game)
+
+
+.org openbus0
+	mov r0,0
+.org openbus1
+	mov r0,0
+.org openbus2
+	mov r0,0
+.org openbus3
+	mov r0,0
+.org openbus4
+	mov r0,0
+.org openbus5
+	mov r0,0
+.org openbus6
+	mov r0,0
+.org openbus7
+	mov r0,0
+.org openbus8
+	mov r0,0
+.org openbus9
+	mov r0,0
+.org openbus10
+	mov r0,0
+.org openbus11
+	mov r0,0
+.org openbus12
+	mov r0,0
